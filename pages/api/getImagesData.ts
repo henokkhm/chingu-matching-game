e2eb716ed072e9db75929faa1cf01eb3be86ebf6
Henok Hailemariam <henokhm2@gmail.com>
2022-11-26 18:04:17 +0300

    Add error handling to getImages api route

--- pages/api/getImagesData.ts
+++ pages/api/getImagesData.ts
@@ -9,16 +9,29 @@ type ImageDataType = {
   }[];
 };
 
+type ErrorMessageType = {
+  message: string;
+};
+
 export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<ImageDataType>
+  res: NextApiResponse<ImageDataType | ErrorMessageType>
 ) {
-  // const { numImages } = req.body;
-  const numImages = 10;
-  const imageIds = generateRandArr(1, 906, numImages);
-  const ImageData = imageIds.map((id) => ({
-    url: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`,
-    id,
-  }));
-  res.status(200).json({ images: ImageData });
+  if (req.method === "GET") {
+    if (req.body.numImages) {
+      const { numImages } = req.body;
+      const imageIds = generateRandArr(1, 906, numImages);
+      const ImageData = imageIds.map((id) => ({
+        url: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`,
+        id,
+      }));
+      res.status(200).json({ images: ImageData });
+    } else {
+      res
+        .status(400)
+        .json({ message: "Please provide the number of images you want" });
+    }
+  } else {
+    res.status(405).json({ message: "Method not supported" });
+  }
 }
